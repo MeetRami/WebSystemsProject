7fb31518d05be70db017fd08c2245143200661f3
Adhitya Maniyan <adhitya.maniyan@gmail.com>
2024-04-17 19:02:07 -0400

    with the tables

--- NFTSite/NFTProject/NFTProject/wsgi.py
+++ NFTSite/NFTProject/NFTProject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for NFTSite project.
+WSGI config for NFTProject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NFTSite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NFTProject.settings")
 
 application = get_wsgi_application()
--- NFTSite/NFTProject/NFTWebsite/apps.py
+++ NFTSite/NFTProject/NFTWebsite/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class NftConfig(AppConfig):
+class NftwebsiteConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "NFT"
+    name = "NFTWebsite"
