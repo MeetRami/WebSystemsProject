2e228616c5bc0b38fda072ce2d8fe4116e81377d
Adhitya Maniyan <adhitya.maniyan@gmail.com>
2024-04-22 14:28:07 -0400

    sold out works too

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -56,7 +56,7 @@ def add_to_cart(request, product_id):
 	cart_item, created = CartItem.objects.get_or_create(product=product, user=request.user)
 	cart_item.quantity = 1
 	cart_item.save()
-	return redirect('cart:view_cart')
+	return redirect('cart:product_list')
 
 def remove_from_cart(request, item_id):
 	cart_item = CartItem.objects.get(id=item_id)
