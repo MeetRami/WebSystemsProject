b9d9ad764dcd27ffd0f23073462ba3b29d29a384
Adhitya Maniyan <adhitya.maniyan@gmail.com>
2024-04-22 06:34:28 -0400

    keeping everything back

--- ecommerce/cart/models.py
+++ ecommerce/cart/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='products/')
     isSold = models.BooleanField(default=False)
+    InCart = models.BooleanField(default=False)
  
     def __str__(self):
         return self.name
--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -6,29 +6,15 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
+
 @login_required
 def product_list(request):
-    
     products = Product.objects.all()
-    cartItems = CartItem.objects.all()
-    totalItems = 0
-    totalItems += sum(item.quantity for item in cartItems)
-    username = request.user.username
-    return render(request, 'index.html', {'products': products, 'totalItems': totalItems, 'username': username})
-
-
-
-def add_to_cart(request, product_id):
-    product = get_object_or_404(Product, id=product_id)
-    
-    if isinstance(request.user, AnonymousUser):
-        return JsonResponse({'message': 'Please log in to add items to your cart'}, status=401)
-    
-    cart_item, created = CartItem.objects.get_or_create(product=product, user=request.user)
-    cart_item.quantity = 1
-    cart_item.save()
-    
-    return JsonResponse({'message': 'Item added to cart successfully'})
+    cart_items = CartItem.objects.filter(user=request.user)
+    cart_products = [item.product for item in cart_items]
+    for product in products:
+        product.InCart = product in cart_products
+    return render(request, 'index.html', {'products': products, 'username': request.user.username})
 
 
 def view_cart(request):
@@ -43,7 +29,7 @@ def add_to_cart(request, product_id):
 	cart_item, created = CartItem.objects.get_or_create(product=product, user=request.user)
 	cart_item.quantity = 1
 	cart_item.save()
-	return JsonResponse({'message': 'Item added to cart'})
+	return redirect('cart:view_cart')
 
 def remove_from_cart(request, item_id):
 	cart_item = CartItem.objects.get(id=item_id)
